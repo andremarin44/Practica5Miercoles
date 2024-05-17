f18be8a9f65c3b95d5f72f9e3b19ae650e4bc4d5
Juliana Castaño <andremarin44@gmail.com>
2024-05-17 11:26:02 -0500

    borradores

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,46 @@
-#include "mainwindow.h"
-#include "ui_mainwindow.h"
+// Clase principal del juego
+class PiedraPapelTijeraGame : public QWidget {
+    Q_OBJECT
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
-}
+public:
+    PiedraPapelTijeraGame(QWidget *parent = nullptr);
 
-MainWindow::~MainWindow()
-{
-    delete ui;
-}
+private slots;
+    void onAgregarTijeras();
+    void onAgregarPiedra();
+    void onAgregarPapel();
+    void onIngresarJugador();
+    void onIniciarJuego();
+    void onTiempoAgotado();
+
+private:
+    // Funciones privadas para la lógica del juego
+    void crearObjeto(TipoObjeto tipo);
+    void moverObjetos();
+    void verificarColisiones();
+    void destruirObjeto(Objeto* objeto);
+    void actualizarPuntajes();
+    void actualizarTiempoRestante();
+
+    // Otros miembros privados necesarios
+    // ...
+};
+
+// Clase para representar los objetos del juego
+class Objeto : public QGraphicsItem {
+    // Implementación de la clase Objeto
+    // ...
+};
+
+// Clase para representar el indicador del jugador
+class IndicadorJugador : public QGraphicsItem {
+    // Implementación de la clase IndicadorJugador
+    // ...
+};
+
+// Clase para representar el temporizador
+class Temporizador : public QTimer {
+    // Implementación de la clase Temporizador
+    // ...
+};
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTimer>
+#include <QGraphicsScene>
+#include "objeto.h" // Suponiendo que tienes una clase Objeto para representar los elementos del juego
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +18,23 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void onAgregarTijeras();
+    void onAgregarPiedra();
+    void onAgregarPapel();
+    void onIngresarJugador();
+    void onIniciarJuego();
+    void onTiempoAgotado();
+    void moverObjetos();
+
 private:
     Ui::MainWindow *ui;
+    QGraphicsScene *scene;
+    QTimer *timer;
+
+    // Otros miembros privados necesarios
+    // ...
 };
+
 #endif // MAINWINDOW_H
+
